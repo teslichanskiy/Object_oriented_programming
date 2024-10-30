3299196910db89a1b77c358337cb9be3d15ef4b1
Danila Teslya <tesla.kyrgyz2022@gmail.com>
2024-10-31 00:33:52 +0300

    Add files via upload

---
+++ C#/lab1(inheritance_and_aggregation_by_link)/lab1(inheritance_and_aggregation_by_link)/classes.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace Lab_1
+{
+    class A
+    {
+        public A(B b, C c, J j)
+        {
+            this.b = b;
+            this.c = c;
+            this.j = j;
+        }
+
+        ~A() { }
+
+        public void mA() { Console.WriteLine("method of A"); }
+        public B bA
+        {
+            set { Console.WriteLine("set b"); b = value; }
+            get { Console.Write("get b ->"); return b; }
+        }
+        public C cA
+        {
+            set { Console.WriteLine("set c"); c = value; }
+            get { Console.Write("get c ->"); return c; }
+        }
+        public J jA
+        {
+            set { Console.WriteLine("set j"); j = value; }
+            get { Console.Write("get j ->"); return j; }
+        }
+
+        private B b = null;
+        private C c = null;
+        private J j = null;
+    }
+
+    class B
+    {
+        public B(D d) { this.d = d; }
+        ~B() { }
+        public void mB() { Console.WriteLine("method of B"); }
+
+        public D dA
+        {
+            set { Console.WriteLine("set d"); d = value; }
+            get { Console.Write("get d ->"); return d; }
+        }
+
+        private D d = null;
+    }
+
+    class C
+    {
+        public C(K k, F f, E e)
+        {
+            this.k = k;
+            this.f = f;
+            this.e = e;
+        }
+
+        ~C() { }
+
+        public void mC() { Console.WriteLine("method of C"); }
+        public K kA
+        {
+            set { Console.WriteLine("set k"); k = value; }
+            get { Console.Write("get k -> "); return k; }
+        }
+        public F fA
+        {
+            set { Console.WriteLine("set f"); f = value; }
+            get { Console.Write("get f -> "); return f; }
+        }
+        public E eA
+        {
+            set { Console.WriteLine("set e"); e = value; }
+            get { Console.Write("get e ->"); return e; }
+        }
+
+        private K k = null;
+        private F f = null;
+        private E e = null;
+
+    }
+
+    class D
+    {
+        public D() { }
+        ~D() { }
+        public void mD() { Console.WriteLine(" method of D"); }
+    }
+
+    class E
+    {
+        public E() { }
+        ~E() { }
+        public void mE() { Console.WriteLine(" method of E"); }
+    }
+
+    class F
+    {
+        public F() { }
+        ~F() { }
+        public void mF() { Console.WriteLine(" method of F"); }
+    }
+
+    class J
+    {
+        public J() { }
+        ~J() { }
+        public void mJ() { Console.WriteLine(" method of J"); }
+    }
+
+    class K
+    {
+        public K() { }
+        ~K() { }
+        public void mK() { Console.WriteLine(" method of K"); }
+    }
+}
---
+++ C#/lab1(inheritance_and_aggregation_by_link)/lab1(inheritance_and_aggregation_by_link)/main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab_1;
+
+class main
+{
+    static void Main(string[] args)
+    {
+        K k = new K();
+        F f = new F();
+        E e = new E();
+        D d = new D();
+        J j = new J();
+        B b = new B(d);
+        C c = new C(k, f, e);
+        A a = new A(b, c, j);
+        a.mA();
+        a.bA.mB();
+        a.cA.mC();
+        a.jA.mJ();
+        a.cA.kA.mK();
+        a.cA.fA.mF();
+        a.cA.eA.mE();
+        a.bA.dA.mD();
+        Console.ReadKey();
+    }
+}
