cbfd428dd0d0d077e6435b129024e5f86a6197b7
Danila Teslya <tesla.kyrgyz2022@gmail.com>
2024-10-31 01:40:45 +0300

    Add files via upload

---
+++ C++/lab0(bot)/ConsoleApplication6/Bot.Teslya.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <string>
+#include "colors.cpp"
+
+
+int main()
+{
+	using namespace std;
+	using namespace clr;
+
+    cout << Color("Hello! My name is Bot. Enter your name please:", blue) << endl;
+	string name;
+	std::getline(std::cin, name);
+	cout << Color("Nice to meet you, ", cyan);
+	cout << Color(name, cyan) << endl;
+	cout << Color("Please answer some questions for me using only one word in each answer.\nWhen you get bored and want to finish, please write 'Goodbye'", green) << endl;
+	string question[15] = { "Where are you from", "How old are you?", "What is your gender?", "What is your job?", "What is your favourite food?", "Which music genre do you enjoy most?", 
+		"What kind of sport do you enjoy doing most?","Do you have some hobbies?", "Do you have enough free time?", "What is your hometown?", "Do you learn some foriegn languages?",
+		"What is your favourite season of the year?"};
+	string answers[10] = {"Very nice man!", "So intresting", "Wow!That's awesome", "Well, glad for you", "Oh...", "Well", "Glad to hear it!", "That was my favourite question and now favourite answer for it!"
+	};
+	string answer;
+	int randquest, randanswer;
+	const string bye = "Goodbye";
+	const string yes = "yes";
+	const string no = "no";
+	const string rus = "Russia";
+	const string rock = "rock";
+	const string pizza = "pizza";
+	const string sum = "summer";
+	const string foot = "football";
+	while (true) {
+		randquest = std::rand() % 12;
+		randanswer = std::rand() % 8;
+		std::cout << Color(name, cyan) << Color(", ", cyan);
+		std::cout << Color(question[randquest], cyan) << endl;
+
+		std::getline(std::cin, answer);
+
+		if (answer == bye) {
+			break;
+		}
+		else {
+			if ((answer == rock && randquest == 5) || (answer == pizza && randquest == 4)) {
+				std::cout << Color("Wow, me too! I'm a big fan of it.", blue) << '\n' << endl;
+			}
+			else {
+				if (answer == yes && randquest == 8) {
+					std::cout << Color("You're lucky", blue) << '\n' << endl;
+				}
+				else {
+					if (answer == no && (randquest == 10 || randquest == 7)) {
+						std::cout << Color("You're weird man...", blue) << '\n' << endl;
+					}
+					else {
+						if (answer == sum && randquest == 11) {
+							std::cout << Color("Mine too!", blue) << '\n' << endl;
+						}
+						else {
+							if (answer == rus) {
+								std::cout << Color("Wow, great country!", blue) << '\n' << endl;
+							}
+							else {
+								if (answer == foot && randquest == 11) {
+									std::cout << Color("Wow, me too", blue) << '\n' << endl;
+								}
+								else {
+									std::cout << Color(answers[randanswer], blue) << '\n' << endl;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	std::cout << Color("Goodbye, ", orange);
+	std::cout << Color(name, orange) << endl;
+	return 0;
+}
---
+++ C++/lab00(overload)/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Complex
+{
+private:
+    double real, imaginary;
+public:
+    Complex() { real = 1; imaginary = 2; }
+    Complex(double re, double im) { real = re, imaginary = im; }
+    friend Complex operator+(const Complex& c1, const Complex& c2); //Перегрузка операторов через дружественные функции
+    friend Complex operator-(const Complex& c1, const Complex& c2);
+    Complex operator*(const Complex& c) const; //Перегрузка операторов через обычные функции
+    Complex operator/(const Complex& c) const;
+    friend ostream& operator<<(ostream& out, const Complex& c); //Перегрузка оператора вывода через дружественную функцию
+};
+
+Complex operator+(const Complex& c1, const Complex& c2)
+{
+    Complex sum;
+    sum.real = c1.real + c2.real;
+    sum.imaginary = c1.imaginary + c2.imaginary;
+    return sum;
+}
+
+Complex operator-(const Complex& c1, const Complex& c2)
+{
+    Complex diff;
+    diff.real = c1.real - c2.real;
+    diff.imaginary = c1.imaginary - c2.imaginary;
+    return diff;
+}
+
+Complex Complex::operator*(const Complex& c) const
+{
+    Complex comp;
+    comp.real = real * c.real + (-1) * imaginary * c.imaginary;
+    comp.imaginary = real * c.imaginary + c.real * imaginary;
+    return comp;
+}
+
+Complex Complex::operator/(const Complex& c) const
+{
+    Complex frac;
+    frac.real = (real * c.real + imaginary * c.imaginary) / (c.real * c.real + c.imaginary * c.imaginary);
+    frac.imaginary = (c.real * imaginary + (-1) * real * c.imaginary) / (c.real * c.real + c.imaginary * c.imaginary);
+    return frac;
+}
+
+ostream& operator<<(ostream& out, const Complex& c)
+{
+    if (c.imaginary == 0)
+        out << c.real << endl;
+    if (c.imaginary == 1)
+        out << c.real << "+i" << endl;
+    if (c.imaginary == -1)
+        out << c.real << "-i" << endl;
+    if (c.imaginary < 0 && c.imaginary != -1)
+        out << c.real << c.imaginary << "i" << endl;
+    if (c.imaginary > 0 && c.imaginary != 1)
+        out << c.real << "+" << c.imaginary << "i" << endl;
+    return out;
+}
+
+int main()
+{
+    Complex c1(3.25, -3.75)/*, c2(3, -1)*/;
+    Complex c2;
+    cout << "c1 = " << c1;
+    cout << "c2 = " << c2 << '\n';
+    cout << "c1 + c2 = " << c1 + c2;
+    cout << "c1 - c2 = " << c1 - c2;
+    cout << "c1 * c2 = " << c1 * c2;
+    cout << "c1 / c2 = " << c1 / c2;
+
+    return 0;
+}
