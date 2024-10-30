5d1c7d9847e50d05f25d44b8f382fb571b9752da
Danila Teslya <tesla.kyrgyz2022@gmail.com>
2024-10-31 00:17:24 +0300

    Add files via upload

---
+++ C#/lab0(simple_classes)/lab0(simple_classes)/fraction.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Lab_0
+{
+
+    class Fraction
+    {
+
+        private int _numerator, _denominator;
+
+        public Fraction() { }
+
+        public Fraction(int _numerator, int _denominator)
+        {
+            this._numerator = _numerator;
+            this._denominator = _denominator;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($" {_numerator}/{_denominator} ");
+        }
+
+        public static Fraction SumOnNumber(Fraction frac, int a)
+        {
+            int newNumerator = frac._numerator + a * frac._denominator;
+            return new Fraction(newNumerator, frac._denominator);
+        }
+
+        public static Fraction DiffOnNumber(Fraction frac, int a)
+        {
+            int newNumerator = frac._numerator - a * frac._denominator;
+            return new Fraction(newNumerator, frac._denominator);
+        }
+
+        public static Fraction MultiplyOnNumber(Fraction frac, int a)
+        {
+            int newNumerator = frac._numerator * a;
+            return new Fraction(newNumerator, frac._denominator);
+        }
+
+        public static Fraction DivisionOnNumber(Fraction frac, int a)
+        {
+            int newDenominator = frac._denominator * a;
+            return new Fraction(frac._numerator, newDenominator);
+        }
+
+
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            int newNumerator = a._numerator * b._denominator + b._numerator * a._denominator;
+            int newDenominator = a._denominator * b._denominator;
+            return new Fraction(newNumerator, newDenominator);
+        }
+
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            int newNumerator = a._numerator * b._denominator - b._numerator * a._denominator;
+            int newDenominator = a._denominator * b._denominator;
+            return new Fraction(newNumerator, newDenominator);
+        }
+
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            int newNumerator = a._numerator * b._numerator;
+            int newDenominator = a._denominator * b._denominator;
+            return new Fraction(newNumerator, newDenominator);
+        }
+
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            if (b._numerator == 0)
+            {
+                throw new DivideByZeroException("Деление на ноль.");
+            }
+            int newNumerator = a._numerator * b._denominator;
+            int newDenominator = a._denominator * b._numerator;
+            return new Fraction(newNumerator, newDenominator);
+        }
+
+        ~Fraction() { }
+    }
+}
---
+++ C#/lab0(simple_classes)/lab0(simple_classes)/main.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab_0;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+class MainProgram
+{
+    static void Main()
+    {
+        var frac = new Fraction(1, 3);
+        var frac1 = new Fraction(2, 8);
+        var frac_res = new Fraction();
+        int a = 4;
+        Console.WriteLine($"Fraction is: ");
+        frac.Print();
+        frac_res = Fraction.SumOnNumber(frac, a);
+        Console.WriteLine($"SumOnNumber is: ");
+        frac_res.Print();
+        frac_res = Fraction.DiffOnNumber(frac, a);
+        Console.WriteLine($"DiffOnNumber is: ");
+        frac_res.Print();
+        frac_res = Fraction.MultiplyOnNumber(frac, a);
+        Console.WriteLine($"MultiplyOnNumber is: ");
+        frac_res.Print();
+        frac_res = Fraction.DivisionOnNumber(frac, a);
+        Console.WriteLine($"DivisionOnNumber is: ");
+        frac_res.Print();
+
+        Console.WriteLine($"\nSecond fraction is: ");
+        frac1.Print();
+        frac_res = frac + frac1;
+        Console.WriteLine($"Summ is: ");
+        frac_res.Print();
+        frac_res = frac - frac1;
+        Console.WriteLine($"Diff is: ");
+        frac_res.Print();
+        frac_res = frac * frac1;
+        Console.WriteLine($"Multiply is: ");
+        frac_res.Print();
+        frac_res = frac / frac1;
+        Console.WriteLine($"Division is: ");
+        frac_res.Print();
+    }
+}
